6cf0cc65be60432a1b8d7a1d110e079ab9324b58
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-21 14:06:18 -0300

    feat: added code style configs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { server } from "./server/Server";
+
 const Port = 3100;
+
 server.listen(Port, () => console.log("rodando"));
--- src/server/Server.ts
+++ src/server/Server.ts
@@ -3,7 +3,7 @@ import express from "express";
 const server = express();
 
 server.get("/", (req, res) => {
-  return res.send("Ola, Dev");
+  return res.send("produto adicionada");
 });
 
 export { server };
