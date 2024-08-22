0efa05e918d2185f6b9a4fcf5376a936ee5b8781
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-21 22:01:38 -0300

    chorus: created a query validation

--- src/server/controllers/city/Create.ts
+++ src/server/controllers/city/Create.ts
@@ -6,10 +6,45 @@ interface ICity {
   city: string;
 }
 
+interface IFilter {
+  filter?: string;
+}
+
 //Validation Schema
 const bodyValidation: yup.Schema<ICity> = yup.object().shape({
   city: yup.string().required().min(3),
 });
+
+//Validation Schema with yup and query
+const queryValidation: yup.Schema<IFilter> = yup.object().shape({
+  filter: yup.string().required().min(3),
+});
+
+//middleware validation with yup and query
+export const createQueryValidator: RequestHandler = async (req, res, next) => {
+  try {
+    await queryValidation.validate(req.query, {
+      abortEarly: false,
+    });
+    //passa para o proximo controller
+    return next();
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach((error) => {
+      error.message;
+      if (!error.path) return;
+
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors,
+    });
+  }
+};
+
 //middleware validation with yup
 export const createBodyValidator: RequestHandler = async (req, res, next) => {
   try {
