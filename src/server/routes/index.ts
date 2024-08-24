a70c88e32320a1d9ed43d969880871b5c084a606
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-24 16:02:50 -0300

    feat: pass get all with a router

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -4,9 +4,10 @@ import { CityController } from "../controllers";
 const router = Router();
 
 router.get("/", (req, res) => {
-  return res.send("produto adicionada");
+  return res.send("product added");
 });
 
 router.post("/city", CityController.createValidation, CityController.create);
+router.get("/city", CityController.getAllValidation, CityController.getAll);
 
 export { router };
