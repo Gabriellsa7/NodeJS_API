830b3d6362d9ce3fee04d80d50b1150be6edc982
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-24 15:28:28 -0300

    feat: improving the create city code

--- src/server/controllers/city/Create.ts
+++ src/server/controllers/city/Create.ts
@@ -1,5 +1,4 @@
-import { Request, RequestHandler, Response } from "express";
-import { StatusCodes } from "http-status-codes";
+import { Request, Response } from "express";
 import * as yup from "yup";
 import { validation } from "../../shared/middleware";
 
@@ -11,18 +10,20 @@ interface IFilter {
   filter?: string;
 }
 
-//Validation Schema
-const bodyValidation: yup.Schema<ICity> = yup.object().shape({
-  city: yup.string().required().min(3),
-});
-
-//Validation Schema with yup and query
-const queryValidation: yup.Schema<IFilter> = yup.object().shape({
-  filter: yup.string().required().min(3),
-});
-
-export const createValidation = validation(queryValidation);
-export const createBodyValidator = validation(bodyValidation);
+export const createValidation = validation((getSchema) => ({
+  body: getSchema<ICity>(
+    yup.object().shape({
+      city: yup.string().required().min(3),
+      filter: yup.string().required().min(3),
+    })
+  ),
+  query: getSchema<IFilter>(
+    yup.object().shape({
+      filter: yup.string().required().min(3),
+    })
+  ),
+}));
+// export const createBodyValidator = validation("body", bodyValidation);
 
 export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
   console.log(req.body);
