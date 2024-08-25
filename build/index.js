a8eb1b9496a8945b32ae886c084be53045035bc7
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-25 14:32:11 -0300

    feat: deleted all build files

--- build/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Server_1 = require("./server/Server");
-Server_1.server.listen(process.env.Port || 3100, () => console.log(`running at the door ${process.env.PORT || 3100}`));
