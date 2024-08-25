1a6fc55c9af3e0916c1b96a941776fee9b4f70b8
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-25 08:00:23 -0300

    feat: pass this methods in CityController

--- src/server/controllers/city/index.ts
+++ src/server/controllers/city/index.ts
@@ -1,9 +1,15 @@
 import * as create from "./Create";
+import * as deleteById from "./DeleteById";
 import * as getAll from "./GetAll";
+import * as getById from "./GetById";
+import * as updateById from "./Update";
 //simplify the use of controls in the routes file
 export const CityController = {
   ...create,
   ...getAll,
+  ...getById,
+  ...updateById,
+  ...deleteById,
 };
 
 CityController.create;
