95e891c701095fea53e7bebebe225c027d8e3b5c
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-25 08:23:03 -0300

    feat: change the phase in portuguese to english

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { server } from "./server/Server";
 
 server.listen(process.env.Port || 3100, () =>
-  console.log(`rodando na porta ${process.env.PORT || 3100}`)
+  console.log(`running at the door ${process.env.PORT || 3100}`)
 );
