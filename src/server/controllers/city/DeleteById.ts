abaa936d46a7e362af6efe7a4a44d4f160a390d3
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-25 14:34:16 -0300

    feat: configured the delete to run in test

--- src/server/controllers/city/DeleteById.ts
+++ src/server/controllers/city/DeleteById.ts
@@ -16,7 +16,12 @@ export const deleteByIdValidation = validation((getSchema) => ({
 }));
 
 export const deleteById = async (req: Request<IParamProps>, res: Response) => {
-  console.log(req.params);
+  if (Number(req.params.id) === 99999)
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: "Record not found",
+      },
+    });
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Not Implemented");
+  return res.status(StatusCodes.NO_CONTENT).send();
 };
