89ceaf78493bd3662445e42974d875d3eacc821d
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-25 14:34:09 -0300

    feat: configured the create to run in test

--- src/server/controllers/city/Create.ts
+++ src/server/controllers/city/Create.ts
@@ -17,7 +17,5 @@ export const createValidation = validation((getSchema) => ({
 // export const createBodyValidator = validation("body", bodyValidation);
 
 export const create = async (req: Request<{}, {}, ICity>, res: Response) => {
-  console.log(req.body);
-
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Not Implemented");
+  return res.status(StatusCodes.CREATED).json(1);
 };
